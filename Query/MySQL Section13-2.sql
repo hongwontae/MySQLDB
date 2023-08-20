37e667b14a079d7f18c2032d7e643d76d87f847b
dnjsxoghd <feelchok1234@gmail.com>
2023-08-20 23:04:56 +0900

    Inner join

--- Query/MySQL Section13-2.sql
+++ Query/MySQL Section13-2.sql
@@ -1,2 +1,14 @@
 -- MySQL Section13-2
---- asd
+
+use mysqlud;
+
+-- Inner Join
+insert into orders (order_date, amount, customer_id) values ('2020-12-25',30.30,3);
+select * from customers;
+select * from orders;
+
+select first_name, last_name, order_date, amount from customers
+join orders on orders.customer_id = customers.id;
+
+select * from orders 
+inner join customers on customers.id = orders.customer_id;
