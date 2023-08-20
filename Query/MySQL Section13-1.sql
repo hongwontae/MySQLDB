b70f451615fd3e0c2381d71c838c2be1feabb53a
dnjsxoghd <feelchok1234@gmail.com>
2023-08-20 19:50:59 +0900

    One To Many PK-FK

--- Query/MySQL Section13-1.sql
+++ Query/MySQL Section13-1.sql
@@ -1,1 +1,34 @@
 -- MySQL Section13-1
+
+use mysqlud;
+
+-- 외래키 다루기
+CREATE TABLE customers (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    email VARCHAR(50)
+); -- Pk가 있는 일반적인 테이블을 생성
+ 
+CREATE TABLE orders (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    order_date DATE,
+    amount DECIMAL(8,2),
+    customer_id INT,
+    FOREIGN KEY (customer_id) REFERENCES customers(id)
+); -- Customers 테이블을 참조하는 테이블을 생성 fk= customer_id
+ 
+INSERT INTO customers (first_name, last_name, email) 
+VALUES ('Boy', 'George', 'george@gmail.com'),
+       ('George', 'Michael', 'gm@gmail.com'),
+       ('David', 'Bowie', 'david@gmail.com'),
+       ('Blue', 'Steele', 'blue@gmail.com'),
+       ('Bette', 'Davis', 'bette@aol.com');
+       
+       
+INSERT INTO orders (order_date, amount, customer_id)
+VALUES ('2016-02-10', 99.99, 1),
+       ('2017-11-11', 35.50, 1),
+       ('2014-12-12', 800.67, 2),
+       ('2015-01-03', 12.50, 2),
+       ('1999-04-11', 450.25, 5);
